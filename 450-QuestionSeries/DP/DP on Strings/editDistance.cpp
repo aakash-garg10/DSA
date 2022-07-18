eaace5f3b50fb6f8bd0999278b9d92fad58fdca6
Aakash Garg <aakashgarg1009@gmail.com>
2022-07-18 11:58:08 +0530

    dp on STRings

---
+++ 450-QuestionSeries/DP/DP on Strings/editDistance.cpp
@@ -0,0 +1,47 @@
+// https://www.codingninjas.com/codestudio/problems/edit-distance_630420?source=youtube&campaign=striver_dp_videos&utm_source=youtube&utm_medium=affiliate&utm_campaign=striver_dp_videos&leftPanelTab=1
+
+//RECURSIVE + MEMOIZATION SOLUTION
+
+// Time Complexity: O(N*M)
+// Space Complexity: O(N*M) + O(N+M)
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int editDistanceUtil(string& S1, string& S2, int i, int j, vector<vector<int>>& dp){
+    
+    if(i<0)
+        return j+1;
+    if(j<0)
+        return i+1;
+        
+    if(dp[i][j]!=-1) return dp[i][j];
+        
+    if(S1[i]==S2[j])
+        return dp[i][j] =  0+editDistanceUtil(S1,S2,i-1,j-1,dp);
+        
+    // Minimum of three choices
+    else return dp[i][j] = 1+min(editDistanceUtil(S1,S2,i-1,j-1,dp),
+    min(editDistanceUtil(S1,S2,i-1,j,dp),editDistanceUtil(S1,S2,i,j-1,dp)));
+    
+}
+
+int editDistance(string& S1, string& S2){
+    
+    int n = S1.size();
+    int m = S2.size();
+    
+    vector<vector<int>> dp(n,vector<int>(m,-1));
+    return editDistanceUtil(S1,S2,n-1,m-1,dp);
+    
+}
+
+int main() {
+
+  string s1 = "horse";
+  string s2 = "ros";
+
+  cout << "The minimum number of operations required is: "<<editDistance(s1,s2);
+}
+
+// Output: The minimum number of operations required is: 3
